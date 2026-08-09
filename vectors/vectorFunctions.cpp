0fe4ff1ee1b85294df2b9f121c6e8aa3e26e80ca
Sandhya Chandel <sandhyachandel83@gmail.com>
2026-08-10 00:57:56 +0530

    Refactor output messages in vectorFunctions.cpp and added capacity() function
    Updated output messages for clarity and consistency in vector operations.

--- vectors/vectorFunctions.cpp
+++ vectors/vectorFunctions.cpp
@@ -12,15 +12,19 @@ int main(){
     }
     
     //size() function
-    cout<<"\nSize of vector: "<<vect.size()<<endl;
+    cout<<"\nSize of vector: "<<vect.size();
+    cout<<"\nCapacity of vector: "<<vect.capacity()<<endl;
     
     //push 20 in a vector
     vect.push_back(20);
     
-    cout<<"vector after push() function: ";
+    cout<<"\nvector after push_back() function: ";
     for (int i: vect){
         cout<<i<<" ";
     }
+    
+    cout<<"\nSize of vector after push_back(): "<<vect.size();
+    cout<<"\nCapacity of vector after push_back(): "<<vect.capacity()<<endl;
     
     //front() function
     cout<<"\nFront of a vector: "<<vect.front();
@@ -33,20 +37,30 @@ int main(){
     
     //pop() function
     vect.pop_back();
-    cout<<"vector after pop() function: ";
+    cout<<"\nVector after pop_back() function: ";
     for (int i: vect){
         cout<<i<<" ";
     }
     
+    cout<<"\nSize of vector after pop_back(): "<<vect.size();
+    cout<<"\nCapacity of vector after pop_back(): "<<vect.capacity()<<endl;
+    
     return 0;
 }
-
 
 /* Output:
 vector: 69 5 0 33 90 
 Size of vector: 5
-vector after push() function: 69 5 0 33 90 20 
+Capacity of vector: 5
+
+vector after push_back() function: 69 5 0 33 90 20 
+Size of vector after push_back(): 6
+Capacity of vector after push_back(): 10
+
 Front of a vector: 69
 Back of a vector: 20
 Vector element at 2nd index: 0
-vector after pop() function: 69 5 0 33 90 */
+
+Vector after pop_back() function: 69 5 0 33 90 
+Size of vector after pop_back(): 5
+Capacity of vector after pop_back(): 10 */
